42500ba834df0f5cdf44f1b4ffd58f6e78417d16
Student <zeidersjack@gmail.com>
2016-10-28 10:11:08 -0700

    Final

---
+++ lab9/art.cpp
@@ -0,0 +1,20 @@
+#include "draw.h"
+#include "turtle.h"
+#include <stdlib.h>
+#include <time.h>
+
+int main(){
+	draw::setpenwidth(10); // thick lines
+	draw::setrange(-100, 100);
+	srand(time(0));
+   Turtle raphael(0, 0, 0);
+   int dist = 0;
+   for(int i = 0; i < 20;i++){
+   		raphael.move(dist);
+   		raphael.turn(90);
+   		raphael.setColor(Color(rand()%255,rand()%255,rand()%255));
+
+   		dist+=10;
+   }
+   return 0;
+}
