ad7ae7fdc343b905bdc15fe51a457b5d93e534b5
Student <zeidersjack@gmail.com>
2016-10-28 10:19:26 -0700

    Test

--- lab9/art.cpp
+++ lab9/art.cpp
@@ -8,9 +8,16 @@ int main(){
 	draw::setrange(-100, 100);
 	srand(time(0));
    Turtle raphael(0, 0, 0);
+  	Turtle a (0,0,0);
+  	Turtle b(0,0,0);
+  	  	Turtle c(0,0,0);
+  	  	b.move(10);
+  	  	c.move(20);
    int dist = 0;
    for(int i = 0; i < 20;i++){
    		raphael.move(dist);
+   		a.move(-dist);
+   		draw::show(100);
    		raphael.turn(90);
    		raphael.setColor(Color(rand()%255,rand()%255,rand()%255));
 
