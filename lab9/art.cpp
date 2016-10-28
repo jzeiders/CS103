ae573a946e38b9db62d56d260c9dd5cfc17ca88d
Jack Zeiders <zeiders@usc.edu>
2016-10-28 10:20:38 -0700

    test

--- lab9/art.cpp
+++ lab9/art.cpp
@@ -7,14 +7,13 @@ int main(){
 	draw::setpenwidth(10); // thick lines
 	draw::setrange(-100, 100);
 	srand(time(0));
-   Turtle raphael(0, 0, 0);
-   int dist = 0;
-   for(int i = 0; i < 20;i++){
-   		raphael.move(dist);
-   		raphael.turn(90);
-   		raphael.setColor(Color(rand()%255,rand()%255,rand()%255));
-
-   		dist+=10;
-   }
-   return 0;
+	Turtle raphael(0, 0, 0);
+	int dist = 0;
+	for(int i = 0; i < 20; i++) {
+		raphael.move(dist);
+		raphael.turn(90);
+		raphael.setColor(Color(rand()%255,rand()%255,rand()%255));
+		dist+=10;
+	}
+	return 0;
 }
--- lab9/turtle.cpp
+++ lab9/turtle.cpp
@@ -6,21 +6,21 @@ Turtle::Turtle(double x0, double y0, double dir0){
 	x= x0;
 	y= y0;
 	dir = dir0;
-  isOn = true;
-  color = draw::BLACK;
+	isOn = true;
+	color = draw::BLACK;
 };
 void Turtle::move(double dist){
-  double x2 = x + cos(dir/180 * M_PI)*dist;
-  double y2 = y + sin(dir/180 * M_PI)*dist;
-  if(isOn){
-  	draw::setcolor(color);
-  	draw::line(x,y,x2,y2);
-  }
-  x = x2;
-  y = y2;
+	double x2 = x + cos(dir/180 * M_PI)*dist;
+	double y2 = y + sin(dir/180 * M_PI)*dist;
+	if(isOn) {
+		draw::setcolor(color);
+		draw::line(x,y,x2,y2);
+	}
+	x = x2;
+	y = y2;
 }
 void Turtle::turn(double angle){
-  dir += angle;
+	dir += angle;
 }
 void Turtle::setColor(Color newColor){
 	color = newColor;
